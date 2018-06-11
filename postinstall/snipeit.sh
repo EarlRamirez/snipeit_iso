cf630a32d91d56fd899ab55f1d7e691734bd8dee
Earl Ramirez <earlaramirez@gmail.com>
2018-06-11 06:27:54 -0400

    Updated for rc.local execution

--- postinstall/snipeit.sh
+++ postinstall/snipeit.sh
@@ -288,7 +288,10 @@ rm -rf /etc/issue
 rm -rf /etc/issue.net
 mv /etc/issue-backup /etc/issue
 mv /etc/issue.net-backup /etc/issue.net
-rm -rf /etc/profile.d/snipeit.sh
+#rm -rf /etc/profile.d/snipeit.sh
+rm -rf /etc/rc.d/rc.local
+/usr/bin/systemctl disable rc-local.service
+mv /etc/rc.d/rc.local-backup /etc/rc.d/rc.local
 echo ""
 echo ""
 echo "It is higly recommended that you change the root default password"
