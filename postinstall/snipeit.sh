2336842a6055fceac9bc5eeccb496ced06015242
Earl Ramirez <earlaramirez@gmail.com>
2018-06-14 16:10:40 -0400

    Clean up for pre-installation

--- postinstall/snipeit.sh
+++ postinstall/snipeit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo " Let's change the default credentials for the system users
-root and snipeit, while we are at it we will change the mariadb root password"
+echo "Let's change the default credentials for the system users
+root and snipeit, while we are at it, we will change the mariadb root password"
 echo ""
 /bin/passwd
 echo ""
@@ -14,6 +14,7 @@ echo ""
 echo "Almost there! Let us conifgure Snipe-IT to send
 email notification, if you wish to perform this
 step at another time run snipeit_mail.setup.sh"
+cd /var/www/htm/snipeit/
 /usr/local/bin/snipeit_mail.setup.sh
 php artisan key:generate --force
 php artisan migrate --force
