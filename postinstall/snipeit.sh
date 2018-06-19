87efc63e8517e886235449359d57605b4570a47a
Earl Ramirez <earlaramirez@gmail.com>
2018-06-18 22:54:34 -0400

    move things around

--- postinstall/snipeit.sh
+++ postinstall/snipeit.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 ipaddr="$(ip -o -4 addr show dev eth0 | sed 's/.* inet \([^/]*\).*/\1/')"
+echo "Final Snipe-IT Configuration"
+# Added key generation and DB migration here
+# because with systemd, there is no order and
+# the DB is getting started after the script
+# is executed
+	cd /var/www/html/snipeit/
+	php artisan key:generate --force
+	php artisan migrate --force
 
 echo "Let's change the default credentials for the system users
 root and snipeit, while we are at it, we will change the mariadb root password"
@@ -18,16 +26,10 @@ echo "Almost there! Let us conifgure Snipe-IT to send
 email notification, if you wish to perform this
 step at another time run snipeit_mail.setup.sh"
 /usr/local/bin/snipeit_mail.setup.sh
-echo "Final Snipe-IT Configuration"
-# Added key generation and DB migration here
-# because with systemd, there is no order and
-# the DB is getting started after the script
-# is executed
-cd /var/www/html/snipeit/
-php artisan key:generate --force
-php artisan migrate --force
 echo ""
-echo "  ***Open http://$ipaddr to login to Snipe-IT.***"
+echo " Point your browser to http://$ipaddr 
+	  to complete Snipe-IT final set-up to start 
+      enjoying Snipe-IT"
 echo ""
 echo "Cleaning up"
 rm -rf /etc/issue
@@ -38,6 +40,9 @@ mv /root/issue /etc/issue.net
 rm -rf /etc/profile.d/snipeit.sh
 systemctl stop rc-local
 systemctl disable rc-local
+echo ""
+echo "$HOST will reboot in 60 seconds"
+echo "rebooting....."
 shutdown -r +1
 sleep 1
 
