69c60a28c55216df3a8f4a1c0b022b9564887a09
Earl Ramirez <earlaramirez@gmail.com>
2018-06-18 23:30:54 -0400

    Order is important

--- postinstall/snipeit.sh
+++ postinstall/snipeit.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 ipaddr="$(ip -o -4 addr show dev eth0 | sed 's/.* inet \([^/]*\).*/\1/')"
-echo "Final Snipe-IT Configuration"
-# Added key generation and DB migration here
-# because with systemd, there is no order and
-# the DB is getting started after the script
-# is executed
-	cd /var/www/html/snipeit/
-	php artisan key:generate --force
-	php artisan migrate --force
 
 echo "Let's change the default credentials for the system users
 root and snipeit, while we are at it, we will change the mariadb root password"
-sleep 10
+sleep 5
 echo ""
 /bin/passwd
 echo ""
@@ -27,11 +19,20 @@ email notification, if you wish to perform this
 step at another time run snipeit_mail.setup.sh"
 /usr/local/bin/snipeit_mail.setup.sh
 echo ""
-echo " Point your browser to http://$ipaddr 
-	  to complete Snipe-IT final set-up to start 
-      enjoying Snipe-IT"
+echo "Final Snipe-IT Configuration"
+# Added key generation and DB migration here
+# because with systemd, there is no order and
+# the DB is getting started after the script
+# is executed
+cd /var/www/html/snipeit/
+php artisan key:generate --force
+php artisan migrate --force
+clear
 echo ""
-echo "Cleaning up"
+echo " Point your browser to http://$ipaddr 
+to complete Snipe-IT final set-up to start enjoying Snipe-IT"
+echo ""
+echo "Final clean up"
 rm -rf /etc/issue
 rm -rf /etc/issue.net
 rm -rf /root/rc.local*
